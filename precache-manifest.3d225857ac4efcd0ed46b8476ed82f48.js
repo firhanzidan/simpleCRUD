0d5baa86a5d23b571cc45b74f511e67960e6bda6
firhanzidan <39345669+firhanzidan@users.noreply.github.com>
2020-09-10 14:45:55 +0700

    Add files via upload

---
+++ precache-manifest.3d225857ac4efcd0ed46b8476ed82f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deaba98f48b3a30207e5f4d3a20038e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "debc83a30ea0e76da9e6",
+    "url": "/static/css/2.9809d50e.chunk.css"
+  },
+  {
+    "revision": "666028b1c7a471ff7ec4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "debc83a30ea0e76da9e6",
+    "url": "/static/js/2.bda6077c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.bda6077c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666028b1c7a471ff7ec4",
+    "url": "/static/js/main.89732907.chunk.js"
+  },
+  {
+    "revision": "1639c5f643d6916a3640",
+    "url": "/static/js/runtime-main.fdc624f9.js"
+  }
+]);
